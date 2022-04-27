c76175ae9f60b3b78455ac85503627f5af4d0129
mrmda28 <mrmda28@gmail.com>
2022-04-28 02:37:36 +0300

    Initial commit without Storyboard

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Dmitry Maslennikov on 28/04/2022.
+//  Copyright © 2022 Dmitry Maslennikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
